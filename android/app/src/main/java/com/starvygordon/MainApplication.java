da62e41449c6664b9c27130eb67f5c83aab8d423
Jesse Jie <jesse_jie@hotmail.com>
2016-10-01 18:01:38 -0400

    add react-native-facebook-login to android manifest, resources and gradle build

--- android/app/src/main/java/com/starvygordon/MainApplication.java
+++ android/app/src/main/java/com/starvygordon/MainApplication.java
@@ -8,7 +8,11 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+<<<<<<< 590a9e6b3f92f01862a4c94a460f106757bd75da
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+=======
+import com.magus.fblogin.FacebookLoginPackage;
+>>>>>>> add react-native-facebook-login to android manifest, resources and gradle build
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new ReactMaterialKitPackage()
+          new ReactMaterialKitPackage(),
+          new FacebookLoginPackage()
       );
     }
   };
